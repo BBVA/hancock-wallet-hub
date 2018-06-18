f777937bf8ecb2479bfd9c5f144c3de615e3ec5c
Juan Francisco Monge <juanfrancisco.monge@bbva.com>
2018-06-18 13:09:13 +0200

    plato config

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     transform: {
         "^.+\\.tsx?$": "ts-jest",
     },
-    testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(tsx?)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverageFrom: [
         "src/**/*.ts",
