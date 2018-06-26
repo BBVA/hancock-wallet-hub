66f2e043379e31a5158dcc9e52bd242f41ba9f0a
Juan Francisco Monge <juanfrancisco.monge@bbva.com>
2018-06-26 12:57:26 +0200

    added lcov

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
     reporters: [ "default", [ "jest-junit", { output: "tests/reports/unit/junit.xml" } ] ],
     coverageDirectory: 'tests/reports/coverage',
     coverageReporters: [
-        "cobertura"
+        "cobertura",
+        "lcov"
     ],
     bail: true,
     coverageThreshold: {
