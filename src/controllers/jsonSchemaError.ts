a52f4388fbb334129f6fea58910d452674acf604
Angel Manzano <angel.manzano.contractor@bbva.com>
2018-07-31 10:40:55 +0200

    Added jsonSchemaValidations error controller

--- src/controllers/jsonSchemaError.ts
+++ src/controllers/jsonSchemaError.ts
@@ -1,15 +1,27 @@
 import { NextFunction, Request, Response } from 'express';
+import { JsonSchemaError, JsonSchemaValidation } from 'express-jsonschema';
 import { hancockBadRequestError } from '../models/error';
 import { error } from '../utils/error';
 
-export function jsonSchemaError(e: Error, req: Request, res: Response, next: NextFunction) {
+export function jsonSchemaError(e: JsonSchemaError, req: Request, res: Response, next: NextFunction) {
+
+  let nextError: Error = e;
 
   if (e.name === 'JsonSchemaValidation') {
 
-    e = error(hancockBadRequestError, e);
+    const newOriginalError: any = new Error();
+
+    newOriginalError.name = e.name;
+    newOriginalError.validations = e.validations;
+    newOriginalError.message = e.validations.body
+      .map((val: JsonSchemaValidation) => `${val.property}[${val.messages.join(', ')}]`)
+      .join(' | ')
+      .replace(/"/g, '\'');
+
+    nextError = error(hancockBadRequestError, newOriginalError);
 
   }
 
-  next(e);
+  next(nextError);
 
 }
--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -1,5 +1,15 @@
 declare module 'express-jsonschema' {
   export function validate(...args: any[]): (...args: any[]) => any;
+  interface JsonSchemaValidation {
+    value: any;
+    property: string;
+    messages: string[];
+  }
+  export interface JsonSchemaError extends Error {
+    validations: {
+      body: JsonSchemaValidation[];
+    }
+  }
 }
 
 declare module 'node-forge';
